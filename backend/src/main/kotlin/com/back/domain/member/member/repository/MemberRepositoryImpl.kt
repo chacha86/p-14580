513fccec58481dc7523f08eb02509db01658e8d5
chacha86 <taejincha0619@gmail.com>
2025-11-06 12:50:16 +0900

    findQById temp

--- backend/src/main/kotlin/com/back/domain/member/member/repository/MemberRepositoryImpl.kt
+++ backend/src/main/kotlin/com/back/domain/member/member/repository/MemberRepositoryImpl.kt
@@ -1,4 +1,11 @@
 package com.back.domain.member.member.repository
 
+import com.back.domain.member.member.entity.Member
+
 class MemberRepositoryImpl: MemberRepositoryCustom {
+
+    override fun findQById(id: Long): Member? {
+        return Member(1,"test","test")
+    }
+
 }
