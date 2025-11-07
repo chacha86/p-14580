c9ee175587eef167d2225a748a7bd7017032bf9b
chacha86 <taejincha0619@gmail.com>
2025-11-07 12:48:33 +0900

    work

--- backend/src/main/kotlin/com/back/domain/member/member/controller/ApiV1AdmMemberController.kt
+++ backend/src/main/kotlin/com/back/domain/member/member/controller/ApiV1AdmMemberController.kt
@@ -2,8 +2,8 @@ package com.back.domain.member.member.controller
 
 import com.back.domain.member.member.dto.MemberWithUsernameDto
 import com.back.domain.member.member.service.MemberService
+import com.back.standard.dto.PageDto
 import io.swagger.v3.oas.annotations.Operation
-import org.springframework.data.domain.Page
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -22,15 +22,13 @@ class ApiV1AdmMemberController(
     fun getItems(
         @RequestParam("page", defaultValue = "1") page: Int,
         @RequestParam("pageSize", defaultValue = "5") pageSize: Int,
-    ): Page<MemberWithUsernameDto> {
+    ): PageDto<MemberWithUsernameDto> {
 
         val page = if (page >= 1) page else 1
         val pageSize = if (pageSize > 30) 30 else pageSize
 
         val memberPage = memberService.findByPaged(page, pageSize)
 
-        return memberPage.map {
-            MemberWithUsernameDto(it)
-        }
+        return PageDto(memberPage.map { MemberWithUsernameDto(it) })
     }
 }
